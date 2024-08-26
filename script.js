fe11a5924bdefc876e70c22943fcf153bb25a498
sumaiya <you@example.com>
2024-08-26 22:54:00 +0600

    script file add and linked up.

---
+++ script.js
@@ -0,0 +1,18 @@
+//*Navbar Section Starts
+$('.first_loop').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+//*Navbar Section Ends
