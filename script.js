eb49a83d2adb36f9a65c580604a72670c9030667
sumaiya <you@example.com>
2024-08-28 13:06:04 +0600

    Navbar with responsive complete

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 $('.first_loop').owlCarousel({
     loop: true,
     margin: 10,
-    nav: true,
+    nav: false,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    smartSpeed: 450,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    dots:false,
     responsive: {
         0: {
             items: 1
@@ -15,4 +22,29 @@ $('.first_loop').owlCarousel({
         }
     }
 })
+//!For Responsive Starts
+$('.second_loop').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    smartSpeed: 450,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+//!For Responsive Ends
 //*Navbar Section Ends
