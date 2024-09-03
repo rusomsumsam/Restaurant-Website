bc486f83f72ee45c0f3f0c5f65e13848e4c1d52f
sumaiya <you@example.com>
2024-09-03 22:55:41 +0600

    Menu cart Sectionn and Responsive Complete

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+//*Menu Cart Starts
+$(function () {
+    $('.menu_container').mixItUp({
+        selectors: {
+            target: '.one',
+            filter: '.filter',
+        },
+        animation: {
+            enable: false,
+            effects: 'fade',
+            duration: 600,
+            easing: 'ease',
+            perspectiveDistance: '0',
+            perspectiveOrigin: '0 0',
+            queue: false,
+            queueLimit: 1,
+            animateChangeLayout: false,
+            animateResizeContainer: false,
+            animateResizeTargets: false,
+            staggerSequence: false,
+            reverseOut: false
+        },
+        // callbacks: {
+        //     onMixLoad: false,
+        //     onMixStart: false,
+        //     onMixBusy: false,
+        //     onMixEnd: false,
+        //     onMixFail: false
+        // },
+        // controls: {
+        //     enable: true,
+        //     live: false,
+        //     toggleFilterButtons: false,
+        //     toggleLogic: 'or',
+        //     activeClass: 'active'
+        // },
+        load: {
+            filter: '.starters'
+        }
+    });
+    $('.filter').on('click', function () {
+        var filterValue = $(this).attr('data-filter');
+        $('.menu_container').mixItUp('filter', filterValue);
+    });
+});
 //*Navbar Section Starts
 $('.first_loop').owlCarousel({
     loop: true,
@@ -48,3 +93,6 @@ $('.second_loop').owlCarousel({
 })
 //!For Responsive Ends
 //*Navbar Section Ends
+
+
+
